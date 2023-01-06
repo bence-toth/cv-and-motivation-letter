8bb6fdc2bb0e91dc90761c2f5a0407e0228b0e0e
Bence A. Tóth <tothab@gmail.com>
2023-01-06 11:11:21 +0100

    Deleted files

--- components/avatar.component.js
+++
@@ -1,6 +0,0 @@
-const Avatar = ({imageURL, name, positionTitle}) => (
-  <div contentEditable className='avatar'>
-    <img className='avatarImage' src={imageURL} />
-    <h1>{name} <small>{positionTitle}</small></h1>
-  </div>
-)
--- components/summary.component.js
+++
@@ -1,6 +0,0 @@
-const Summary = ({children}) => (
-  <div contentEditable className="summary">
-    <h2>Profile</h2>
-    {children}
-  </div>
-)
